27eec5be63e46a6086f805dcac159777e16ecc6b
imran <muhammadimransuleman9@gmail.com>
2026-04-27 00:09:38 +0500

    day1 problem 2 completed

---
+++ day1/p2.py
@@ -0,0 +1,12 @@
+'''
+Take a number and print positive, negative, or zero
+'''
+
+num=int(input("Enter a number"))
+
+if(num>0):
+    print("positive")
+elif(num<0):
+    print("negative")
+else:
+    print("zero")
