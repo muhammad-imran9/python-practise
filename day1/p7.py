9a5e02265bf6fab0a3515c1b90150421541c4afa
imran <muhammadimransuleman9@gmail.com>
2026-04-27 01:12:00 +0500

    day1 problem 7 completed

---
+++ day1/p7.py
@@ -0,0 +1,11 @@
+"""
+Print the factorial of a number
+"""
+num=int(input("Enter a number: "))
+
+product=1
+sum=0
+for i in range(num,1,-1):
+    product=product*(i)
+    
+print(f"factorial of a {num} is {product}")
