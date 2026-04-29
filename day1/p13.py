ace144f2c3eeb4722fce0ff0026d3bfe9eaba8b4
imran <muhammadimransuleman9@gmail.com>
2026-04-29 20:52:19 +0500

    day1 problem 13 completed

---
+++ day1/p13.py
@@ -0,0 +1,116 @@
+"""
+Find the frequency of each character
+"""
+
+text=input("Enter any text: ")
+lowertexts=text.lower()
+
+a=b=c=d=e=f=g=h=ii=j=k=l=m=n=o=p=q=r=s=t=u=v=w=x=y=z=0
+for i in range(len(text)):
+    if(lowertexts[i]=='a'):
+        a=a+1
+    if(lowertexts[i]=='b'):
+        b=b+1
+    if(lowertexts[i]=='c'):
+        c=c+1
+    if(lowertexts[i]=='d'):
+        d=d+1
+    if(lowertexts[i]=='e'):
+        e=e+1
+    if(lowertexts[i]=='f'):
+        f=f+1
+    if(lowertexts[i]=='g'):
+        g=g+1
+    if(lowertexts[i]=='h'):
+        h=h+1
+    if(lowertexts[i]=='i'):
+        ii=ii+1
+    if(lowertexts[i]=='j'):
+        j=j+1
+    if(lowertexts[i]=='k'):
+        k=k+1
+    if(lowertexts[i]=='l'):
+        l=l+1
+    if(lowertexts[i]=='m'):
+        m=m+1
+    if(lowertexts[i]=='n'):
+        n=n+1
+    if(lowertexts[i]=='o'):
+        o=o+1
+    if(lowertexts[i]=='p'):
+        p=p+1
+    if(lowertexts[i]=='q'):
+        q=q+1
+    if(lowertexts[i]=='r'):
+        r=r+1
+    if(lowertexts[i]=='s'):
+        s=s+1
+    if(lowertexts[i]=='t'):
+        t=t+1
+    if(lowertexts[i]=='u'):
+        u=u+1
+    if(lowertexts[i]=='v'):
+        v=v+1
+    if(lowertexts[i]=='w'):
+        w=w+1
+    if(lowertexts[i]=='x'):
+        x=x+1
+    if(lowertexts[i]=='y'):
+        y=y+1
+    if(lowertexts[i]=='z'):
+        z=z+1
+
+if(a>0):
+    print(f"frequency of a in the text is {a} ")
+if(b>0):
+    print(f"frequency of b in the text is {b} ")
+if(c>0):
+    print(f"frequency of c in the text is {c} ")
+if(d>0):
+    print(f"frequency of d in the text is {d} ")
+if(e>0):
+    print(f"frequency of e in the text is {e} ")
+if(f>0):
+    print(f"frequency of f in the text is {f} ")
+if(g>0):
+    print(f"frequency of g in the text is {g} ")
+if(h>0):
+    print(f"frequency of h in the text is {h} ")
+if(ii>0):
+    print(f"frequency of i in the text is {ii} ")
+if(j>0):
+    print(f"frequency of j in the text is {j} ")
+if(k>0):
+    print(f"frequency of k in the text is {k} ")
+if(l>0):
+    print(f"frequency of l in the text is {l} ")
+if(m>0):
+    print(f"frequency of m in the text is {m} ")
+if(n>0):
+    print(f"frequency of n in the text is {n} ")
+if(o>0):
+    print(f"frequency of o in the text is {o} ")
+if(p>0):
+    print(f"frequency of p in the text is {p} ")
+if(q>0):
+    print(f"frequency of q in the text is {q} ")
+if(r>0):
+    print(f"frequency of r in the text is {r} ")
+if(s>0):
+    print(f"frequency of s in the text is {s} ")
+if(t>0):
+    print(f"frequency of t in the text is {t} ")
+if(u>0):
+    print(f"frequency of u in the text is {u} ")
+if(v>0):
+    print(f"frequency of v in the text is {v} ")
+if(w>0):
+    print(f"frequency of a in the text is {w} ")
+if(x>0):
+    print(f"frequency of x in the text is {x} ")
+if(y>0):
+    print(f"frequency of y in the text is {y} ")
+if(z>0):
+    print(f"frequency of z in the text is {z} ")
+
+
